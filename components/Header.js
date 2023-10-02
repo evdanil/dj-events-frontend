eeaee428d5ee6348bb7eb333332bad3fc1c021f3
Kvazimodo <evdanil@gmail.com>
2023-10-02 22:09:09 +1000

    jsconfig and path optimisation

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,17 @@
 import Link from 'next/link'
-import styles from '../styles/Header.module.css'
+import styles from '@/styles/Header.module.css'
 
 function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Link href='/'>
-          {/* <a>DJ Events</a> */}
-          DJ Events
-        </Link>
+        <Link href='/'>DJ Events</Link>
       </div>
 
       <nav>
         <ul>
           <li>
-            <Link href='/events'>
-              {/* <a>Events</a> */}
-              Events
-            </Link>
+            <Link href='/events'>Events</Link>
           </li>
         </ul>
       </nav>
